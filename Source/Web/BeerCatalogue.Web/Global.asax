ea3e7a13d10f4865f067186db4247c7fd98a433a
ahansb <andrejboyadjiev@gmail.com>
2016-02-08 13:15:51 +0200

    Initial Commit

---
+++ Source/Web/BeerCatalogue.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BeerCatalogue.Web.MvcApplication" Language="C#" %>
---
+++ Source/Web/BeerCatalogue.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BeerCatalogue.Web.Startup))]
+namespace BeerCatalogue.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
