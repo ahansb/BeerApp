5cfd69447d2b6505a2add32618942d6ee0653887
ahansb <andrejboyadjiev@gmail.com>
2016-02-08 20:36:45 +0200

    merging?

---
+++ Source/BeerCatalogue.Data/BeerCatalogueDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BeerCatalogue.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerCatalogue.Data
+{
+    public class BeerCatalogueDbContext : IdentityDbContext<User>
+    {
+        public BeerCatalogueDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static BeerCatalogueDbContext Create()
+        {
+            return new BeerCatalogueDbContext();
+        }
+    }
+}
---
+++ Source/Web/BeerCatalogue.Web/App_Start/DatabaseConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace BeerCatalogue.Web
+{
+    using Data;
+    using System.Data.Entity;
+    using Data.Migrations;
+
+    public static class DatabaseConfig
+    {
+        public static void Initialize()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<BeerCatalogueDbContext, Configuration>());
+
+            BeerCatalogueDbContext.Create().Database.Initialize(true);
+        }
+    }
+}
