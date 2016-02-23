e11621a607478d36bdac5c1dd12d02162ef027b2
ahansb <andrejboyadjiev@gmail.com>
2016-02-23 14:44:33 +0200

    Fixing Place/All - bug

--- Source/Web/BeerApp.Web/Controllers/PlaceController.cs
+++ Source/Web/BeerApp.Web/Controllers/PlaceController.cs
@@ -1,20 +1,20 @@
 ﻿namespace BeerApp.Web.Controllers
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Web.Mvc;
     using Data.Models;
     using Services.Data;
     using Services.Web;
-    using ViewModels.Place;
-    using System.Linq;
-    using System.Collections.Generic;
     using ViewModels.Country;
+    using ViewModels.Place;
+
     [Authorize]
     public class PlaceController : BaseController
     {
         private readonly IPlacesService places;
         private readonly ICountriesService countries;
         private readonly IIdentifierProvider identifier;
-
 
         public PlaceController(IPlacesService places, IIdentifierProvider identifier, ICountriesService countries)
         {
@@ -28,9 +28,9 @@
         {
             var places = this.places.GetAll().OrderBy(p => p.Country.Name).ToList();
 
-            ICollection<PlaceResponseViewModel> placesView = this.Mapper.Map<ICollection<PlaceResponseViewModel>>(places);
+            var placesView = this.Mapper.Map<ICollection<PlaceResponseViewModel>>(places);
 
-            return this.View(places);
+            return this.View(placesView);
         }
 
         [HttpGet]
@@ -38,7 +38,7 @@
         {
             var place = this.places.GetById(id);
 
-            PlaceResponseViewModel placeView = this.Mapper.Map<PlaceResponseViewModel>(place);
+            var placeView = this.Mapper.Map<PlaceResponseViewModel>(place);
 
             return this.View(placeView);
         }
@@ -51,7 +51,7 @@
             var model = new PlaceRequestViewModel();
             model.Countries = viewCountries;
 
-            return View(model);
+            return this.View(model);
         }
 
         [HttpPost]
