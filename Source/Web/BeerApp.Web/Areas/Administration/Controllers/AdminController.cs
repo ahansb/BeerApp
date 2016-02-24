965c36db103cc82fa76a69dce0436981fa34b57a
ahansb <andrejboyadjiev@gmail.com>
2016-02-24 02:37:49 +0200

    admin menu added

---
+++ Source/Web/BeerApp.Web/Areas/Administration/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿namespace BeerApp.Web.Areas.Administration.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    [Authorize(Roles = "Admin")]
+    [ValidateInput(false)]
+    public class AdminController : Controller
+    {
+        // GET: Administration/Admin/Menu
+        [ChildActionOnly]
+        public ActionResult Menu()
+        {
+            IEnumerable<string> items = new List<string>() { "Beers", "BeerTypes", "Countries", "Places", "Recipes" };
+            return this.PartialView("_AdminMenu", items);
+        }
+    }
+}
